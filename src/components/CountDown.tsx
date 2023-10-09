ef707435f3ddf8c05718958369f96d9c4dc8a9c3
Uday Pawar <udaysinghpawar0302@gmail.com>
2023-10-09 11:51:10 +0530

    added backend functionality (partial)

--- src/components/CountDown.tsx
+++ src/components/CountDown.tsx
@@ -18,7 +18,7 @@ const CountDown = () => {
 
   return (
     <Countdown
-      date={new Date("2023-09-29")}
+      date={new Date("2023-10-29")}
       className="font-bold text-xl md:text-5xl text-yellow-300"
     />
   );
--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,11 +1,55 @@
-import { NextAuthOptions } from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { NextAuthOptions, User, getServerSession } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { PrismaClient } from "@prisma/client";
+
+declare module "next-auth" {
+  interface Session {
+    user: User & {
+      isAdmin: Boolean;
+    };
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    isAdmin: Boolean;
+  }
+}
+
+const prisma = new PrismaClient();
 
 export const authOptions: NextAuthOptions = {
+  //to add user to database use prisma adapter
+  adapter: PrismaAdapter(prisma),
+  session: {
+    strategy: "jwt",
+  },
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID as string,
       clientSecret: process.env.GOOGLE_SECRET as string,
     }),
   ],
+  callbacks: {
+    async session({ token, session }) {
+      if (token) {
+        session.user.isAdmin = token.isAdmin;
+      }
+      return session;
+    },
+    async jwt({ token }) {
+      const userInDb = await prisma.user.findUnique({
+        where: {
+          email: token.email!,
+        },
+      });
+      token.isAdmin = userInDb?.isAdmin!;
+      return token;
+    },
+  },
 };
+
+//using this function we can get details about
+//user data and status in the server side components
+export const getAuthSession = () => getServerSession(authOptions);
